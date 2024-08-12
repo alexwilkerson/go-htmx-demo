67ccedd67f4e37df5ab3caa9654c589ca88006a1
Alex Wilkerson <alexwilkerson@users.noreply.github.com>
2024-08-12 14:05:07 -0600

    chapter 2 checkpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	count := 0
+
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, New York Times!"))
+		http.ServeFile(w, r, "./templates/index.html")
 	})
+
+	http.HandleFunc("GET /button", func(w http.ResponseWriter, r *http.Request) {
+		count++
+
+		w.Write([]byte(fmt.Sprintf("<p>Count: %d</p>", count)))
+	})
+
+	http.Handle("GET /static/*",
+		http.StripPrefix("/static/",
+			http.FileServer(
+				http.Dir("./static"),
+			),
+		),
+	)
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
