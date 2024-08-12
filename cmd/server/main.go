bb61ab08b6bfc97ae31a7ee4125200e4176433ce
Alex Wilkerson <alexwilkerson@users.noreply.github.com>
2024-08-12 13:18:16 -0600

    chapter 1 checkpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, New York Times!"))
 	})
 
