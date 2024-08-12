488f3824de3604f9f9aa90f1b0a01b2178052fa5
Alex Wilkerson <alexwilkerson@users.noreply.github.com>
2024-08-12 12:52:44 -0600

    chapter 1 checkpoint

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, New York Times!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexwilkerson/go-htmx-demo
+
+go 1.22.6
