3670ef888384af06720c69973c276793d22b5808
Alex Wilkerson <alexwilkerson@users.noreply.github.com>
2024-08-13 10:55:59 -0600

    chapter 5 checkpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
@@ -15,6 +16,8 @@ func main() {
 	count := 0
 	sessionManager := scs.New()
 	sessionManager.Lifetime = 24 * time.Hour
+	leaderboard := map[string]int{}
+	chat := []components.Message{}
 
 	mux := http.NewServeMux()
 
@@ -28,6 +31,42 @@ func main() {
 		components.Index(user).Render(r.Context(), w)
 	})
 
+	mux.HandleFunc("GET /chat", func(w http.ResponseWriter, r *http.Request) {
+		const chatSize = 10
+
+		window := r.URL.Query().Get("window")
+
+		if window == strings.ToLower("true") {
+			if len(chat) > chatSize {
+				chat = chat[len(chat)-chatSize:]
+			}
+
+			components.ChatWindow(chat).Render(r.Context(), w)
+			return
+		}
+
+		components.Chat().Render(r.Context(), w)
+	})
+
+	mux.HandleFunc("POST /chat", func(w http.ResponseWriter, r *http.Request) {
+		user := sessionManager.GetString(r.Context(), "user")
+		message := r.FormValue("message")
+
+		if message == "" {
+			return
+		}
+
+		chat = append(chat, components.Message{
+			User:    user,
+			Message: message,
+			Time:    time.Now(),
+		})
+	})
+
+	mux.HandleFunc("GET /leaderboard", func(w http.ResponseWriter, r *http.Request) {
+		components.Leaderboard(leaderboard).Render(r.Context(), w)
+	})
+
 	mux.HandleFunc("GET /button", func(w http.ResponseWriter, r *http.Request) {
 		user := sessionManager.GetString(r.Context(), "user")
 		if user == "" {
@@ -39,6 +78,8 @@ func main() {
 
 		sessionCount := sessionManager.GetInt(r.Context(), "count")
 		sessionManager.Put(r.Context(), "count", sessionCount+1)
+
+		leaderboard[user] = sessionCount + 1
 
 		w.Write([]byte(fmt.Sprintf("<p>User: %s</p><p>Global Count: %d</p><p>Session Count: %d</p>", user, count, sessionCount+1)))
 	})
